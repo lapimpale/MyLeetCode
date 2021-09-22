15d46a2c3cac222919ff34766cb8f8ed73a9ce91
Lalit Pimpale <lalitpimpale07@gmail.com>
2021-09-22 13:00:04 +0000

    Sync LeetCode submission - Fibonacci Number (java)

---
+++ problems/fibonacci_number/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int fib(int n) {
+        
+       {
+        // Base Case
+        if (n <= 1)
+            return n;
+  
+        // Recursive call
+        return fib(n - 1)
+            + fib(n - 2);
+    }
+  }
+}
