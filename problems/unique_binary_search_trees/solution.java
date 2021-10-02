2e2c56983c3b043460e87d1fea37468c35bc9618
Lalit Pimpale <lalitpimpale07@gmail.com>
2021-10-02 07:05:30 +0000

    Sync LeetCode submission - Unique Binary Search Trees (java)

---
+++ problems/unique_binary_search_trees/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numTrees(int n) {
+        int[] ways = new int[n+1];
+        ways[0] = 1; ways[1] = 1;
+        return calc(n,ways);
+    }
+    public int calc(int n, int[] ways){
+        if(ways[n] != 0) return ways[n];
+        int totalWays = 0;
+        for(int i=1; i<=n; i++){
+            totalWays += calc(i-1,ways)*calc(n-i,ways);
+        }
+        return ways[n] = totalWays;
+    }
+}
