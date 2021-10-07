231808b31a636279d6681a2a0125f8abb54f828b
Lalit Pimpale <lalitpimpale07@gmail.com>
2021-10-07 12:07:58 +0000

    Sync LeetCode submission - Maximal Square (java)

---
+++ problems/maximal_square/solution.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+         int m = matrix.length;
+        int n = matrix[0].length;
+        int ans = 0;
+        int dp[][] = new int[m][n];
+        
+        for(int i = m-1;i>=0;i--){
+            for(int j = n-1;j>=0;j--){
+                if(i == m-1 && j == n-1){
+                    dp[i][j] = matrix[i][j] - '0';
+                }else if(i == m-1){
+                    dp[i][j] = matrix[i][j] - '0';
+                }else if(j == n-1){
+                    dp[i][j] = matrix[i][j] - '0';
+                }else{
+                    if(matrix[i][j] == '0'){
+                        dp[i][j] = 0;
+                    }else{
+                        dp[i][j] = 1 +  Math.min(dp[i+1][j] , Math.min(dp[i+1][j+1],dp[i][j+1]));
+                    }
+                }
+                ans = Math.max(ans,dp[i][j]);
+            }
+        }
+        
+        return ans*ans;
+    }
+}
