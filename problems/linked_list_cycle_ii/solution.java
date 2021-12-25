9c58ac0849ab4a0e829b3064f8517551a74c4f48
Lalit Pimpale <lalitpimpale07@gmail.com>
2021-12-25 08:17:29 +0000

    Sync LeetCode submission - Linked List Cycle II (java)

---
+++ problems/linked_list_cycle_ii/solution.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+     public ListNode detectCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        ListNode ptr1 = null;
+        ListNode ptr2 = null;
+        while(fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if(slow == head) return head;
+            if(fast == slow) {
+                ptr1 = head;
+                ptr2 = slow;
+                while(ptr1 != ptr2) {
+                    ptr1 = ptr1.next;
+                    ptr2 = ptr2.next;
+                }
+                return ptr1;
+            }
+        }
+        
+        return null;
+     }
+}
