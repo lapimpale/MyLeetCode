812c0eb0c1ca6343772fdbdd06378992efefff9f
Lalit Pimpale <lalitpimpale07@gmail.com>
2022-01-04 05:38:24 +0000

    Sync LeetCode submission - Complement of Base 10 Integer (java)

---
+++ problems/complement_of_base_10_integer/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int bitwiseComplement(int num) {
+        //base case
+        if(num == 0) return 1;
+         // calculating the mask
+    int x = num;     // say num = 100000
+      x |= x >> 1;    // 100000 | 010000 = 110000
+      x |= x >> 2;    // 110000 | 001100 = 111100
+      x |= x >> 4;    // 111100 | 000011 = 111111
+      x |= x >> 8;    // 111111 | 000000 = 111111
+      x |= x >> 16;    // 111111 | 000000 = 111111
+  
+    return (num ^ x); // 100000 | 111111 = 011111
+    }
+}
