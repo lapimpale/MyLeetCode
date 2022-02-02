c4b403056a3bc26ba4e60ffd9baf84fc7751da8c
Lalit Pimpale <lalitpimpale07@gmail.com>
2022-02-02 04:53:12 +0000

    Sync LeetCode submission - Find All Anagrams in a String (java)

--- problems/find_all_anagrams_in_a_string/solution.java
+++ problems/find_all_anagrams_in_a_string/solution.java
@@ -1,42 +1,21 @@
 class Solution {
-    public static List<Integer> findAnagrams(String s, String p) {
-
-    if (s == null || s.length() == 0 || p == null || p.length() == 0 || p.length() > s.length()) {
-        return new ArrayList<>();
-    }
-
-    List<Integer> result = new ArrayList<>();
-
-    int[] input = new int[26];
-    int[] pattern = new int[26];
-    for (int i = 0; i < p.length(); i++) {
-        pattern[p.charAt(i) - 'a']++;
-        input[s.charAt(i) - 'a']++;
-    }
-
-    if (isAnagram(pattern, input)) {
-        result.add(0);
-    }
-    int j = 0;
-
-    int inputCount = 1;
-    while (inputCount + p.length() - 1 < s.length()) {
-        input[s.charAt(inputCount - 1) - 'a']--;
-        input[s.charAt(inputCount + p.length() - 1) - 'a']++;
-        if (isAnagram(pattern, input)) {
-            result.add(inputCount);
+     public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> ret = new ArrayList<>();
+        int window = p.length();
+        for (int i=0;i<=s.length()-window;i++){
+            String substr = s.substring(i,i+window);
+            if (isAnagram(substr,p)) ret.add(i);
         }
-        inputCount++;
+        return ret;
     }
-    return result;
-}
 
-private static boolean isAnagram(int[] pattern, int[] input) {
-    for (int i = 0; i < 26; i++) {
-        if (pattern[i] != input[i]) {
-            return false;
+    public boolean isAnagram(String s1, String s2){
+        int [] arr = new int[26];
+        for (int i=0;i<s1.length();i++){
+            arr[s1.charAt(i)-'a']++;
+            arr[s2.charAt(i)-'a']--;
         }
+        for (int i=0;i<26;i++)if (arr[i]!=0)return false;
+        return true;
     }
-    return true;
-}
 }
