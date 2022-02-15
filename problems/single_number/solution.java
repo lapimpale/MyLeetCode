41f8de132d0723197f174e73a258fb0210d8ad9a
Lalit Pimpale <lalitpimpale07@gmail.com>
2022-02-15 03:54:37 +0000

    Sync LeetCode submission - Single Number (java)

--- problems/single_number/solution.java
+++ problems/single_number/solution.java
@@ -1,20 +1,10 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int count=0;
-        for(int i=0;i<nums.length-1;i++)
-        {
-            for(int j=i+1;j<nums.length;j++)
-            {
-                if(nums[i]==nums[j])
-                {
-                    nums[i]=0;
-                    nums[j]=0;
-                }
-            }
-        }
+        int singleNumber =0;
         
-        for(int j=0;j<nums.length;j++)
-            count=count+nums[j];
-        return count;
+        for(int i=0;i<nums.length;i++){
+            singleNumber ^=nums[i];
+            }
+        return singleNumber;
     }
 }
