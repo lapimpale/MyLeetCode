0c3dfde93ccaacaa84e207ef536f4f19678c455a
Lalit Pimpale <lalitpimpale07@gmail.com>
2022-02-22 05:50:03 +0000

    Sync LeetCode submission - Excel Sheet Column Number (java)

--- problems/excel_sheet_column_number/solution.java
+++ problems/excel_sheet_column_number/solution.java
@@ -1,9 +1,12 @@
 class Solution {
     public int titleToNumber(String columnTitle) {
-        
-        int n=columnTitle.length(),c=0;
-     for(int i=0;i<n;i++)
-      c+=Math.pow(26,n-i-1)*(columnTitle.charAt(i)-'A'+1);
-      return c;
+        if (columnTitle == null) return -1;
+        int sum = 0;
+        // for each loop so we don't need to mess with index values.
+        for (char c : columnTitle.toUpperCase().toCharArray()) {
+            sum *= 26;
+            sum += c - 'A' + 1;
+        }
+        return sum;
     }
 }
