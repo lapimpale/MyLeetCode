ed38ea6c08d1bffb62869895a7e3bd188649b6a2
Lalit Pimpale <lalitpimpale07@gmail.com>
2022-03-01 09:29:04 +0000

    Sync LeetCode submission - Letter Combinations of a Phone Number (java)

--- problems/letter_combinations_of_a_phone_number/solution.java
+++ problems/letter_combinations_of_a_phone_number/solution.java
@@ -1,21 +1,17 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
-       List<String> res = new ArrayList<>();
-        if (digits == null || digits.equals(""))
-            return res;
-        String[] map = new String[]{null, null, "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-        backtrack(0, new StringBuilder(), digits, map, res);
-        return res;
-    }
-
-    private void backtrack(int curr, StringBuilder str, String digits, String[] map, List<String> res) {
-        if (str.length() == digits.length()) {
-            res.add(str.toString());
-            return;
-        }
-        for (char c : map[digits.charAt(curr) - '0'].toCharArray()) {
-            backtrack(curr + 1, str.append(c), digits, map, res);
-            str.deleteCharAt(str.length() - 1); //Always delete the char added above since str is shared between recursion runs
-        } 
+		LinkedList<String> ans = new LinkedList<String>();
+		if(digits.isEmpty()) return ans;
+		String[] mapping = new String[] {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+		ans.add("");
+		for(int i =0; i<digits.length();i++){
+			int x = Character.getNumericValue(digits.charAt(i));
+			while(ans.peek().length()==i){
+				String t = ans.remove();
+				for(char s : mapping[x].toCharArray())
+					ans.add(t+s);
+			}
+		}
+		return ans;
     }
 }
