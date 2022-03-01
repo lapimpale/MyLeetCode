68b9c3f500381867e3298c0d2b296576276e3fd8
Lalit Pimpale <lalitpimpale07@gmail.com>
2022-03-01 10:44:53 +0000

    Sync LeetCode submission - Number of Islands (java)

--- problems/number_of_islands/solution.java
+++ problems/number_of_islands/solution.java
@@ -1,31 +1,31 @@
-class Solution {
-   public int numIslands(char[][] grid) {
-        int ans = 0;
-        for (int r = 0; r < grid.length; r++) {
-            for (int c = 0; c < grid[0].length; c++) {
-                if (grid[r][c] == '1') {
-                    ans++;
-                    dfs(grid, r, c);
+public class Solution {
+
+    private int n;
+    private int m;
+    
+    public int numIslands(char[][] grid) {
+        int count = 0;
+        n = grid.length;
+        if (n == 0) return 0;
+        m = grid[0].length;
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < m; j++)
+                if (grid[i][j] == '1') {
+                    DFSMarking(grid, i, j);
+                    ++count;
                 }
-            }
-        }
-        return ans;
+        }    
+        return count;
     }
-    
-    int[] dr = new int[] {1, -1, 0, 0};
-    int[] dc = new int[] {0, 0, 1, -1};
-    
-    void dfs(char[][] grid, int r, int c) {
-        if (!isValid(grid, r, c)) {
-            return;
-        }
-        grid[r][c] = '0';
-        for (int i = 0; i < 4; i++) {
-            dfs(grid, r + dr[i], c + dc[i]);
-        }
-    }
-    
-    boolean isValid(char[][] grid, int r, int c) {
-        return 0 <= r && r < grid.length && 0 <= c && c < grid[0].length && grid[r][c] == '1';
+
+    private void DFSMarking(char[][] grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= n || j >= m || grid[i][j] != '1') return;
+        
+        grid[i][j] = '0';
+        DFSMarking(grid, i + 1, j);
+        DFSMarking(grid, i - 1, j);
+        DFSMarking(grid, i, j + 1);
+        DFSMarking(grid, i, j - 1);
+        
     }
 }
