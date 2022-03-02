58ee9553a75e055ee542b2edfde7d69cdfe56792
Lalit Pimpale <lalitpimpale07@gmail.com>
2022-03-02 06:41:23 +0000

    Sync LeetCode submission - Is Subsequence (java)

--- problems/is_subsequence/solution.java
+++ problems/is_subsequence/solution.java
@@ -1,16 +1,20 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        int sPointer = 0, tPointer = 0;
-        
-        while (sPointer < s.length() && tPointer < t.length()) {
-            if (s.charAt(sPointer) == t.charAt(tPointer)) {
-                sPointer ++;
-                tPointer ++;
-            } else {
-                tPointer++;
-            }
+        List<Integer>[] idx = new List[256]; // Just for clarity
+        for (int i = 0; i < t.length(); i++) {
+            if (idx[t.charAt(i)] == null)
+                idx[t.charAt(i)] = new ArrayList<>();
+            idx[t.charAt(i)].add(i);
         }
         
-        return (sPointer == s.length());
+        int prev = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (idx[s.charAt(i)] == null) return false; // Note: char of S does NOT exist in T causing NPE
+            int j = Collections.binarySearch(idx[s.charAt(i)], prev);
+            if (j < 0) j = -j - 1;
+            if (j == idx[s.charAt(i)].size()) return false;
+            prev = idx[s.charAt(i)].get(j) + 1;
+        }
+        return true;
     }
 }
