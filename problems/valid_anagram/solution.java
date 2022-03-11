096868e816557c0fd5083d693fd6383ec3d52948
Lalit Pimpale <lalitpimpale07@gmail.com>
2022-03-11 05:12:53 +0000

    Sync LeetCode submission - Valid Anagram (java)

--- problems/valid_anagram/solution.java
+++ problems/valid_anagram/solution.java
@@ -1,12 +1,9 @@
-class Solution {
+public class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) {
-            return false;
-        }
-        char[] firstArray = s.toCharArray();
-        char[] secondArray = t.toCharArray();
-        Arrays.sort(firstArray);
-        Arrays.sort(secondArray);
-        return Arrays.equals(firstArray, secondArray);
+        int[] alphabet = new int[26];
+        for (int i = 0; i < s.length(); i++) alphabet[s.charAt(i) - 'a']++;
+        for (int i = 0; i < t.length(); i++) alphabet[t.charAt(i) - 'a']--;
+        for (int i : alphabet) if (i != 0) return false;
+        return true;
     }
 }
