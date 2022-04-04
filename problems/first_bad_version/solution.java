e3ecb83fe6521984463be42ff70bc970029a9577
Lalit Pimpale <lalitpimpale07@gmail.com>
2022-04-04 04:34:56 +0000

    Sync LeetCode submission - First Bad Version (java)

--- problems/first_bad_version/solution.java
+++ problems/first_bad_version/solution.java
@@ -3,24 +3,12 @@
 
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-        int left = 1,
-            right = n;
-        while (left <= right) {
-            int mid = left + ((right - left) / 2);
-            if (isFirstBadVersion(mid)) {
-                return mid;
-            }
-            if (isBadVersion(mid)) {
-                right = mid - 1;
-            } else {
-                left = mid + 1;
-            }
-        }
-        return n;
-    }
-    
-    private boolean isFirstBadVersion(int version) {
-        return isBadVersion(version) &&
-            (version == 1 || !isBadVersion(version - 1));
+         int start = 1, end = n;
+    while (start < end) {
+        int mid = start + (end-start) / 2;
+        if (!isBadVersion(mid)) start = mid + 1;
+        else end = mid;            
+    }        
+    return start;
     }
 }
