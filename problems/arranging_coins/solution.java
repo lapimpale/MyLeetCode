bc2be92846059248fb8e90729e5e4fc7df5ecac3
Lalit Pimpale <lalitpimpale07@gmail.com>
2022-04-05 07:02:57 +0000

    Sync LeetCode submission - Arranging Coins (java)

--- problems/arranging_coins/solution.java
+++ problems/arranging_coins/solution.java
@@ -1,35 +1,19 @@
 class Solution {
-    public int arrangeCoins(int n) {
-       int current=1;
+  public int arrangeCoins(int n) {
+    long left = 0, right = n;
+    long k, curr;
+    while (left <= right) {
+      k = left + (right - left) / 2;
+      curr = k * (k + 1) / 2;
 
-        int noOfRowsTillNow=0;
+      if (curr == n) return (int)k;
 
-        for(current=1; n-current>=0 ; current++)
-
-        {
-
-            n=n-current;
-
-            noOfRowsTillNow++;
-
-        }
-
-       /* same logic with while loop
-
-       while(n-current>=0)
-
-        {
-
-            n=n-current;
-
-            noOfRowsTillNow++;
-
-            current++;
-
-        }
-
-       */
-
-        return noOfRowsTillNow; 
+      if (n < curr) {
+        right = k - 1;
+      } else {
+        left = k + 1;
+      }
     }
+    return (int)right;
+  }
 }
