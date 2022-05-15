8a2436999186bb973f65764243eb5f89154f5c94
Lalit Pimpale <lalitpimpale07@gmail.com>
2022-05-15 04:03:23 +0000

    Sync LeetCode submission - Maximum Consecutive Floors Without Special Floors (java)

---
+++ problems/maximum_consecutive_floors_without_special_floors/solution.java
@@ -0,0 +1,11 @@
+class Solution {
+
+	public int maxConsecutive(int bottom, int top, int[] special) {
+		Arrays.sort(special);
+		int max = 0;
+		for (int i = 1; i < special.length; i++) {
+			max = Math.max(max, special[i] - special[i - 1] - 1);
+		}
+		return Math.max(max, Math.max(special[0] - bottom, top - special[special.length - 1]));
+	}
+}
